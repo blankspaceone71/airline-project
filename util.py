7ff39170ff15d3e3aaa78d1cb539d3f63cb457f5
blankspaceone71 <72721757+blankspaceone71@users.noreply.github.com>
2021-11-11 23:12:17 +0530

    Update util.py

--- util.py
+++ util.py
@@ -2,7 +2,8 @@ import numpy as np
 import pickle
 import json
 
-__data_columns = None
+__data_columns = ["air asia", "air india", "goair", "indigo", "jet airways", "jet airways business", "multiple carriers", "multiple carriers premium economy", "spicejet", "vistara", "vistara premium economy", "source", "destination", "additional_info", "total_stops", "day_of_journey", "month_of_journey", "dep_hr", "dep_min"]
+
 __model = None
 
 
@@ -78,16 +79,24 @@ def load_artifacts():
     global __data_columns
     global __model
 
-    with open('./artifacts/column.json', 'r') as f:
-        __data_columns = json.load(f)['data_columns']
+    #with open('./artifacts/column.json', 'r') as f:
+        #__data_columns = json.load(f)['data_columns']
+        #return __data_columns
 
     # global __model
     with open('./artifacts/airline_model.pickle', 'rb') as g:
         __model = pickle.load(g)
+        #return __model
+
+    #print(__data_columns)
 
 
+#print(__data_columns)
 def get_data_columns():
     return __data_columns
 
 if __name__ == '__main__':
     load_artifacts()
+
+
+
